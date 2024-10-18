2566606c6d2b2d2f873a2b3908a48815e401a53f
Deiu <d3iu25@gmail.com>
2024-10-18 17:00:51 +0300

    theme change localStorage 18:50 course

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,9 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 export default function Home() {
   return (
     <div>
-       <UserButton afterSwitchSessionUrl='/' />
+       
     </div>
   )
 }
