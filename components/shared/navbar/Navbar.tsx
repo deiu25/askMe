ba1b3a42ea946a0d8ad37394ff02829d3cd87a03
Deiu <d3iu25@gmail.com>
2024-10-22 14:32:27 +0300

    navbar search, mobile nav

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,41 +3,44 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Theme from './Theme'
+import MobileNav from './MobileNav'
+import GlobalSearch from '../search/GlobalSearch'
 
 const Navbar = () => {
   return (
-    <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
-        <Link href="/" className='flex items-center gap-1'>
-            <Image
-                src="/assets/images/site-logo.svg"
-                width={23}
-                height={23}
-                alt="AskMe"
-            />
-            <p className='h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden'>AskMe <span className='text-primary-500'>Anything</span></p>
-        </Link>
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow"
+        />
 
-        GlobalSearch
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Ask <span className="text-primary-500">Me</span></p>
+      </Link>
 
-        <div className='flex-between gap-5'>
-            <Theme />
+      <GlobalSearch />
 
-            <SignedIn>
-                <UserButton 
-                    afterSwitchSessionUrl='/' 
-                    appearance={{
-                        elements: {
-                            avatarBox: 'h-10 w-10'
-                        },
-                        variables: {
-                            colorPrimary: '#ff700'
-                        }
-                    }}
-                />
-            </SignedIn>
+      <div className="flex-between gap-5">
+        <Theme />
 
-            MobileNavigation
-        </div>
+        <SignedIn>
+          <UserButton 
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: 'h-10 w-10'
+              },
+              variables: {
+                colorPrimary: '#ff7000'
+              }
+            }}
+          />
+        </SignedIn>
+
+        <MobileNav />
+      </div>
     </nav>
   )
 }
